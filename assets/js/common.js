fc0914c7b510f75d2456e1030530a3243fa1c16e
zinpaihtet <zinpainghtet@qualymm.com>
2023-05-30 14:27:24 +0630

    js update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,14 @@
 const AccorTitles = Array.from(document.querySelectorAll(".accor_item"));
 
-AccorTitles.forEach(function (title) {
+const removeClassFromOthers = (array, indx) => {
+  array.forEach(function (item, index) {
+    if (indx !== index) item.classList.remove("active_item");
+  });
+};
+
+AccorTitles.forEach(function (title, indx) {
   title.addEventListener("click", function () {
+    removeClassFromOthers(AccorTitles, indx);
     this.classList.toggle("active_item");
   });
 });
